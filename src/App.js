86e53ba9736d8cae6db0f9a9d248ba1a80ee68e7
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-10-01 17:13:39 -0700

    basic styling lightmode darkmode

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import './App.css';
-import { Box, Heading, Container } from '@chakra-ui/react';
+import { Box, Heading, Container, Flex, IconButton, useColorMode } from '@chakra-ui/react';
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import QuestionRandomizer from './QuestionRandomizer';
 
-
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
+
   return (
-    <Container centerContent>
-      <Box padding="6" boxShadow="lg">
-        <Heading as="h1" size="xl" marginBottom="4">
+    <Container>
+      <Flex gap={2} p={4}>
+        <Heading as="h1" size="md">
           Grokking The Coding Interview Randomizer
         </Heading>
+        <IconButton
+          icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
+          isRound
+          size="md"
+          onClick={toggleColorMode}
+          aria-label={`Switch to ${
+            colorMode === 'dark' ? 'light' : 'dark'
+          } mode`}
+        />
+      </Flex>
+
+      <Box padding="6" boxShadow="lg">
         <QuestionRandomizer />
       </Box>
     </Container>
   );
 }
-
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  config: {
+    initialColorMode: "dark",
+    useSystemColorMode: false,
+  },
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
+    <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <App />
   </ChakraProvider>,
 );
