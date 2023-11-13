46d0d436629428288dd2875651215ce464f3995a
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-11-12 16:24:34 -0800

    feat: basic navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,23 @@
 import './App.css';
 import { Box, Heading, Container, Flex, IconButton, useColorMode } from '@chakra-ui/react';
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
 import QuestionRandomizer from './QuestionRandomizer';
+import Navbar from './Navbar'
 
 function App() {
-  const { colorMode, toggleColorMode } = useColorMode();
-
   return (
-    <Container>
-      <Flex gap={4} p={4}>
-        <Heading as="h1" size="md" whiteSpace="nowrap">
-          Grokking The Coding Interview Randomizer
-        </Heading>
-        <IconButton
-          icon={colorMode === 'dark' ? <SunIcon /> : <MoonIcon />}
-          isRound
-          size="md"
-          onClick={toggleColorMode}
-          aria-label={`Switch to ${
-            colorMode === 'dark' ? 'light' : 'dark'
-          } mode`}
-        />
-      </Flex>
-
-      <Box padding="6" boxShadow="lg">
-        <QuestionRandomizer />
-      </Box>
-    </Container>
+    <Box>
+      <Navbar/>
+      <Container>
+        <Box padding="6" boxShadow="lg">
+          <Flex gap={4} p={4}>
+            <Heading as="h1" size="md">
+              Grokking The Coding Interview Randomizer
+            </Heading>
+          </Flex>
+          <QuestionRandomizer />
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
