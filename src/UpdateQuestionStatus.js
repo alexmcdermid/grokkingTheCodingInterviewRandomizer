ee8b41401a94ff0df86e27ca396db5723c1c772c
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-11-17 15:44:26 -0800

    fix: dont ping firebase unless needed

--- src/UpdateQuestionStatus.js
+++ src/UpdateQuestionStatus.js
@@ -6,6 +6,10 @@ export const useUpdateQuestionStatus = () => {
   const { db } = useFirebase();
 
   const updateQuestionStatus = async (href, startedAt, completed) => {
+    // guard clause for the time being as this is null by default
+    // and we can only update from null to not null
+    if (!completed) return
+
     try {
       const q = query(collection(db, "userProblemTracking"), where("link", "==", href), where("clickedAt", "==", startedAt));
       const querySnapshot = await getDocs(q);
