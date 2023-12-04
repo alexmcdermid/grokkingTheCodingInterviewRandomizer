3a7f321fab8fa0f120a0a2b466b98675bdcfd208
Alex McDermid <alexander.mcdermid@alumni.ubc.ca>
2023-12-04 15:48:57 -0800

    fix: difficulty list

--- src/Questions.js
+++ src/Questions.js
@@ -2,149 +2,149 @@
 
 const questions = {
   "Sliding Window": [
-    { url: "https://leetcode.com/problems/maximum-subarray/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/minimum-size-subarray-sum/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/fruit-into-baskets/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/longest-substring-without-repeating-characters/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/longest-repeating-character-replacement/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/max-consecutive-ones-iii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/permutation-in-string/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-all-anagrams-in-a-string/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/minimum-window-substring/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/substring-with-concatenation-of-all-words/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/maximum-subarray/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/minimum-size-subarray-sum/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/fruit-into-baskets/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/longest-substring-without-repeating-characters/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/longest-repeating-character-replacement/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/max-consecutive-ones-iii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/permutation-in-string/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/find-all-anagrams-in-a-string/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/minimum-window-substring/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/substring-with-concatenation-of-all-words/", difficulty: "hard" }
   ],
   "Two Pointers": [
     { url: "https://leetcode.com/problems/two-sum/", difficulty: "easy" },
     { url: "https://leetcode.com/problems/remove-duplicates-from-sorted-array/", difficulty: "easy" },
     { url: "https://leetcode.com/problems/squares-of-a-sorted-array/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/3sum/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/3sum-closest/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/3sum-smaller/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/subarray-product-less-than-k/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/sort-colors/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/4sum/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/3sum/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/3sum-closest/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/3sum-smaller/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/subarray-product-less-than-k/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/sort-colors/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/4sum/", difficulty: "medium" },
     { url: "https://leetcode.com/problems/backspace-string-compare/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/shortest-unsorted-continuous-subarray/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/shortest-unsorted-continuous-subarray/", difficulty: "medium" }
   ],
   "Fast & Slow pointers": [
     { url: "https://leetcode.com/problems/linked-list-cycle/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/linked-list-cycle-ii/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/linked-list-cycle-ii/", difficulty: "medium" },
     { url: "https://leetcode.com/problems/happy-number/", difficulty: "easy" },
     { url: "https://leetcode.com/problems/middle-of-the-linked-list/", difficulty: "easy" },
     { url: "https://leetcode.com/problems/palindrome-linked-list/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/reorder-list/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/circular-array-loop/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/reorder-list/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/circular-array-loop/", difficulty: "medium" }
   ],
   "Merge Intervals": [
-    { url: "https://leetcode.com/problems/merge-intervals/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/insert-interval/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/interval-list-intersections/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/meeting-rooms-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/employee-free-time/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/merge-intervals/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/insert-interval/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/interval-list-intersections/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/meeting-rooms-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/employee-free-time/", difficulty: "hard" }
   ],  
   "Cyclic Sort": [
     { url: "https://leetcode.com/problems/missing-number/discuss/859510/C%2B%2B-O(N)-O(1)-using-Cyclic-Sort", difficulty: "easy" },
     { url: "https://leetcode.com/problems/missing-number/", difficulty: "easy" },
     { url: "https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-all-duplicates-in-an-array/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-the-duplicate-number/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/find-all-duplicates-in-an-array/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/find-the-duplicate-number/", difficulty: "medium" },
     { url: "https://leetcode.com/problems/missing-number/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/first-missing-positive/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/first-missing-positive/", difficulty: "hard" },
     { url: "https://leetcode.com/problems/kth-missing-positive-number/", difficulty: "easy" }
   ],
   "In-place Reversal of a LinkedList": [
     { url: "https://leetcode.com/problems/reverse-linked-list/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/reverse-linked-list-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/reverse-nodes-in-k-group/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/rotate-list/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/reverse-linked-list-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/reverse-nodes-in-k-group/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/rotate-list/", difficulty: "medium" }
   ],
   "Tree Breadth First Search": [
     { url: "https://leetcode.com/problems/path-sum/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/path-sum-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/sum-root-to-leaf-numbers/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/description/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/path-sum-iii/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/path-sum-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/sum-root-to-leaf-numbers/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/description/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/path-sum-iii/", difficulty: "medium" },
     { url: "https://leetcode.com/problems/diameter-of-binary-tree/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/binary-tree-maximum-path-sum/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/binary-tree-maximum-path-sum/", difficulty: "hard" }
   ],
   "Tree Depth First Search": [
     { url: "https://leetcode.com/problems/path-sum/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/path-sum-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/sum-root-to-leaf-numbers/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/description/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/path-sum-iii/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/path-sum-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/sum-root-to-leaf-numbers/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree/description/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/path-sum-iii/", difficulty: "medium" },
     { url: "https://leetcode.com/problems/diameter-of-binary-tree/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/binary-tree-maximum-path-sum/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/binary-tree-maximum-path-sum/", difficulty: "hard" }
   ],
   "Two Heaps": [
-    { url: "https://leetcode.com/problems/find-median-from-data-stream/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/sliding-window-median/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/ipo/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-right-interval/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/find-median-from-data-stream/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/sliding-window-median/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/ipo/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/find-right-interval/", difficulty: "medium" }
   ],
   "Subsets": [
-    { url: "https://leetcode.com/problems/subsets/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/subsets-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/permutations/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/letter-case-permutation/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/generate-parentheses/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/generalized-abbreviation/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/different-ways-to-add-parentheses/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/unique-binary-search-trees-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/unique-binary-search-trees/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/subsets/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/subsets-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/permutations/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/letter-case-permutation/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/generate-parentheses/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/generalized-abbreviation/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/different-ways-to-add-parentheses/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/unique-binary-search-trees-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/unique-binary-search-trees/", difficulty: "medium" }
   ],
   "Modified Binary Search": [
     { url: "https://leetcode.com/problems/binary-search/", difficulty: "easy" },
     { url: "https://leetcode.com/problems/find-smallest-letter-greater-than-target/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-k-closest-elements/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/peak-index-in-a-mountain-array/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-in-mountain-array/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/search-in-rotated-sorted-array/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/search-in-a-sorted-array-of-unknown-size/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/find-k-closest-elements/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/peak-index-in-a-mountain-array/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/find-in-mountain-array/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/search-in-rotated-sorted-array/", difficulty: "medium" }
   ],
   "Bitwise XOR": [
     { url: "https://leetcode.com/problems/single-number/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/single-number-iii/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/single-number-iii/", difficulty: "medium" },
     { url: "https://leetcode.com/problems/complement-of-base-10-integer/", difficulty: "easy" },
     { url: "https://leetcode.com/problems/flipping-an-image/", difficulty: "easy" }
   ],
   "Top 'K' elements": [
-    { url: "https://leetcode.com/problems/kth-largest-element-in-an-array", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/k-closest-points-to-origin/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/minimum-cost-to-connect-sticks/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/top-k-frequent-elements/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/sort-characters-by-frequency/", difficulty: "easy" },
+    { url: "https://leetcode.com/problems/kth-largest-element-in-an-array", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/k-closest-points-to-origin/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/minimum-cost-to-connect-sticks/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/top-k-frequent-elements/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/sort-characters-by-frequency/", difficulty: "medium" },
     { url: "https://leetcode.com/problems/kth-largest-element-in-a-stream/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-k-closest-elements/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/reorganize-string/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/rearrange-string-k-distance-apart/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/task-scheduler/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/maximum-frequency-stack/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/find-k-closest-elements/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/reorganize-string/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/rearrange-string-k-distance-apart/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/task-scheduler/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/maximum-frequency-stack/", difficulty: "hard" }
   ],
   "K-way merge": [
-    { url: "https://leetcode.com/problems/merge-k-sorted-lists/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/find-k-pairs-with-smallest-sums/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/merge-k-sorted-lists/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/smallest-range-covering-elements-from-k-lists/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/find-k-pairs-with-smallest-sums/", difficulty: "medium" }
   ],
   "0/1 Knapsack": [
-    { url: "https://leetcode.com/problems/partition-equal-subset-sum/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/last-stone-weight-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/combination-sum-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/target-sum/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/ones-and-zeroes/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/partition-equal-subset-sum/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/last-stone-weight-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/combination-sum-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/target-sum/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/ones-and-zeroes/", difficulty: "medium" }
   ],
   "Topological Sort": [
-    { url: "https://leetcode.com/problems/course-schedule/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/course-schedule-ii/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/alien-dictionary/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/sequence-reconstruction/", difficulty: "easy" },
-    { url: "https://leetcode.com/problems/minimum-height-trees/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/course-schedule/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/course-schedule-ii/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/alien-dictionary/", difficulty: "hard" },
+    { url: "https://leetcode.com/problems/sequence-reconstruction/", difficulty: "medium" },
+    { url: "https://leetcode.com/problems/minimum-height-trees/", difficulty: "medium" }
   ],
   "Misc": [
-    { url: "https://leetcode.com/problems/kth-largest-element-in-an-array/", difficulty: "easy" }
+    { url: "https://leetcode.com/problems/kth-largest-element-in-an-array/", difficulty: "medium" }
   ]
 };
 
